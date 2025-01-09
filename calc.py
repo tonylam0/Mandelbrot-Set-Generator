7480f621f3148a0571799d468384547c4b284fb9
Tonycs <tonycs1247@gmail.com>
2025-01-08 19:12:39 -0500

    Cleaned up code

--- calc.py
+++ calc.py
@@ -1,15 +1,16 @@
 import numpy
 import matplotlib
 
-# Iterative formula
-# c represents a complex number
-def calc(x, c):
-    return x**2 + c
-
-# Initial value
-x = 0.8
-n = 0
-
-while n < 20:
-    x = calc(x, 0.15j)
-    n += 1
+class Bounds:
+    def __init__(self, initial, complex_num, iterations) -> None:
+        self.initial = initial
+        self.complex_num = complex_num
+        self.iterations = iterations
+    
+    # Iterative formula
+    # c represents a complex number 
+    def calc(initial, complex_num):
+       return initial**2 + complex_num
+    
+    def draw(points: list):
+        pass
