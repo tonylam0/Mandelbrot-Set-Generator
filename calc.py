a44d5a885e7509f667d5b4a563c41aeffcefe174
Tonycs <tonycs1247@gmail.com>
2025-01-16 20:53:08 -0500

    Added higher power setting option

--- calc.py
+++ calc.py
@@ -4,12 +4,13 @@ import pygame
 
 class Bounds:
     SCALE = 350
+    power = 2
 
     # Creates a range for real and imaginary numbers
     # Increasing the amount of points increases the accuracy
-    points = 1000
-    real_range = np.linspace(-2, 1, points)
-    imag_range = np.linspace(-1.5, 1.5, points)
+    points = 1000 * power / 2
+    real_range = np.linspace(-2, 1, int(points))
+    imag_range = np.linspace(-1.5, 1.5, int(points))
 
     # Creates a 2D grid for the complex plane
     real, imag = np.meshgrid(real_range, imag_range)
@@ -17,43 +18,56 @@ class Bounds:
     # Outputs a list of lists containing real array + imaginary array * j
     c_values = real + imag * 1j
     iterations = np.zeros(c_values.shape)
+    coordinates = {}
 
     escape_radius = 2
+
+    # Used to update values when power is set
+    def update():
+        Bounds.points = 1000 + Bounds.power * 50
+        real_range = np.linspace(-2, 1, int(Bounds.points))
+        imag_range = np.linspace(-1.5, 1.5, int(Bounds.points))
+
+        real, imag = np.meshgrid(real_range, imag_range)
+
+        Bounds.c_values = real + imag * 1j
+        Bounds.iterations = np.zeros(Bounds.c_values.shape)
+        Bounds.coordinates = {}
     
     # Iterative formula
     # c represents a complex number 
     def calc():
-        # Will return 500 for each due to range 
+        # Will return numbers of points in ranges
         for real in range(Bounds.c_values.shape[0]):
             print(f"Computing: {int(real/Bounds.points*100+1)}% done")
             for imag in range(Bounds.c_values.shape[1]):
                 z = 0
                 c = Bounds.c_values[real, imag]  # Treated as coordinate in the 2D plane
                 max_iterations = 100
+                Bounds.coordinates[(real, imag)] = 0
 
                 for i in range(max_iterations):
-                    z = z**2 + c
+                    z = z**Bounds.power + c
                     
                     # Tracks when z unbounds
                     # i > 0 prevents escaped c's to be colored the same as bounded
                     if abs(z) > Bounds.escape_radius and i > 0:
                         Bounds.iterations[real, imag] = i
-                        # print(f"Point {c.round(2)} escaped at iteration {i}")
+                        Bounds.coordinates[real, imag] = i
                         break
     
     def draw(win):
-        for real in range(Bounds.c_values.shape[0]):
-            for imag in range(Bounds.c_values.shape[1]):
-                c = Bounds.c_values[real, imag] 
-                color = (
-                    Bounds.iterations[real, imag] * 5 % 255, 
-                    Bounds.iterations[real, imag] * 10 % 255, 
-                    Bounds.iterations[real, imag] * 15 % 255
-                )
+        for coordinate, i in Bounds.coordinates.items():
+            c = Bounds.c_values[coordinate[0], coordinate[1]] 
+            color = (
+                i * 5 % 255, 
+                i * 10 % 255, 
+                i * 15 % 255
+            )
 
-                pygame.draw.circle(
-                    win, 
-                    color, 
-                    (c.real * Bounds.SCALE + 1400 / 2, c.imag * Bounds.SCALE + 800 / 2),
-                    1
-                )
+            pygame.draw.circle(
+                win, 
+                color, 
+                (c.real * Bounds.SCALE + 1400 / 2, c.imag * Bounds.SCALE + 800 / 2),
+                1
+            )
--- main.py
+++ main.py
@@ -7,13 +7,41 @@ pygame.init()
 WINDOW = pygame.display.set_mode((1000,800))
 pygame.display.set_caption("Mandelbrot Set Generator")
 
+def check_type(x):
+    try:
+        int(x)
+        return int
+    except ValueError:
+        try:
+            float(x)
+            return float
+        except ValueError:
+            return str
+
+def power():
+    while True:
+        power = input("WHAT POWER OF THE MANDELBROT SET DO YOU WANT TO SEE: ")
+
+        if check_type(power) == int:
+            Bounds.power = int(power)
+            Bounds.update()
+            Bounds.calc()
+            break
+        elif check_type(power) == float:
+            Bounds.power = float(power)
+            Bounds.update()
+            Bounds.calc()
+            break
+        else:
+            print("ERROR: PICK A NUMBER")
+
 def main():
     running = True
     clock = pygame.time.Clock()
-    Bounds.calc()
+    power()
 
     while running:
-        clock.tick(20)
+        clock.tick(5)
         WINDOW.fill((255,255,255))
 
         Bounds.draw(WINDOW)
